15373bfd85e7ba8116d7765a2e6328bb8bbf118d
donlino97 <chamas_hadi@hotmail.com>
2023-11-04 14:37:48 +0200

    first commit

---
+++ app/api/user/route.js
@@ -0,0 +1,18 @@
+import prisma from '../../../lib/prismadb'
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  try {
+
+    const users = await prisma.user.findMany();
+
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
---
+++ app/users/page.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+export const getData=async()=>{
+    const res = await fetch(`${process.env.API_URL}/api/user`)
+    return res.json()
+}
+
+const page = async() => {
+    const users = await getData()
+    console.log(users)
+  return (
+    <div>
+        {users.map(user=>user.id)}
+    </div>
+  )
+}
+
+export default page
