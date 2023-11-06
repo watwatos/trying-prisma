0220ef2c41d6c827a16f3f30dc600a675b0f7aca
donlino97 <chamas_hadi@hotmail.com>
2023-11-06 09:51:33 +0200

    ll

--- app/page.js
+++ app/page.js
@@ -6,8 +6,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
-      <Link href={'/users'}>clik</Link>
-      {/* <Us/> */}
+      {/* <Link href={'/users'}>clik</Link>
+      <Us/> */}
+      hello
      
     </main>
   )
--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,17 +1,18 @@
 
 import React from 'react'
 
-export const getData=async()=>{
-    const res = await fetch(`${process.env.API_URL}/api/user`)
-    return res.json()
-}
+// export const getData=async()=>{
+//     const res = await fetch(`${process.env.API_URL}/api/user`)
+//     return res.json()
+// }
 
 const page = async() => {
-    const users = await getData()
-    console.log(users)
+    // const users = await getData()
+    // console.log(users)
   return (
     <div>
-        {users.map(user=>user.id)}
+        {/* {users.map(user=>user.id)} */}
+        hi
     </div>
   )
 }
