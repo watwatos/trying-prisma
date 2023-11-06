6317dc1df68c6c4849b77c9d2e79cbce36f602a8
donlino97 <chamas_hadi@hotmail.com>
2023-11-06 12:24:22 +0200

    ss

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,9 +1,9 @@
-import {prisma} from '../../../lib/prismadb'
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
-
+    const prisma = new PrismaClient();
     const users = await prisma.user.findMany();
 
     return new NextResponse(JSON.stringify(users), { status: 200 });
